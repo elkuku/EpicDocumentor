a633a3b461c322547ce42991391a0241c56ef75b
Nikolai Plath <der.el.kuku@gmail.com>
2014-02-22 09:37:43 -0500

    Avoid a PHP warning if composer hasn't been setup yet

--- www/index.php
+++ www/index.php
@@ -13,14 +13,18 @@ define('JPATH_CONFIGURATION', JPATH_ROOT . '/etc');
 define('JPATH_THEMES',        JPATH_ROOT . '/www');
 define('JPATH_TEMPLATES',     JPATH_ROOT . '/tpl');
 
+$path = realpath(JPATH_ROOT . '/vendor/autoload.php');
+
 // Load the Composer autoloader
-if (false == include JPATH_ROOT . '/vendor/autoload.php')
+if (!$path)
 {
 	echo 'ERROR: Composer not properly set up! Run "composer install" or see README.md for more details.' . PHP_EOL;
 
 	exit(1);
 }
 
+include JPATH_ROOT . '/vendor/autoload.php';
+
 // Execute the application.
-with(new Epicdoc\Application)
+(new Epicdoc\Application)
 	->execute();
